cf31d0d2defc75029f4c74ddf16da9b3f9735629
nirgluzman <nir.gluzman@gmail.com>
2024-06-05 21:32:26 +0200

    create a container to render each Lottie animation

---
+++ components/LottieContainer.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function LottieContainer({ name, path }) {
+  return (
+    <View>
+      <Text>Hello, App is Ready !!</Text>
+    </View>
+  );
+}
