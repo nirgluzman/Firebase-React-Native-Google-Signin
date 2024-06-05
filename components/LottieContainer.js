374cdf849a72e94c0439fd5d5c9aea6319152f19
nirgluzman <nir.gluzman@gmail.com>
2024-06-05 23:17:23 +0200

    add animation render after app loading

--- components/LottieContainer.js
+++ components/LottieContainer.js
@@ -1,9 +1,38 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 
-export default function LottieContainer({ name, path }) {
+import LottieView from 'lottie-react-native';
+
+export default function LottieContainer({ name, src }) {
+  const [show, setShow] = useState(true);
+
   return (
-    <View>
-      <Text>Hello, App is Ready !!</Text>
-    </View>
+    <>
+      <Button title={name} onPress={() => setShow(prev => !prev)} />
+      <View style={styles.lottieContainer}>
+        {show && (
+          <LottieView
+            style={styles.animationContainer}
+            source={src}
+            autoPlay // auto play whenever the component is rendered.
+            loop={true} // true = loop forever.
+            resizeMode='contain' // resize the animation to fit the container.
+          />
+        )}
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  lottieContainer: {
+    width: '200',
+    height: '200',
+    margin: 10
+  },
+
+  animationContainer: {
+    width: '100%',
+    height: 200
+  }
+});
--- screens/Home.js
+++ screens/Home.js
@@ -5,18 +5,21 @@ import {
   StyleSheet
 } from 'react-native';
 
+// Animation library
+import Animated, { FadeIn } from 'react-native-reanimated';
+
 import LottieContainer from '../components/LottieContainer';
 import { animations } from '../constants/lottieAnimations';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text style={styles.title}>A Glimpse to Lottie Animations</Text>
+      <Text style={styles.title}>A Glimpse to Lottie Animations</Text>
+      <Animated.ScrollView entering={FadeIn.duration(500)}>
         {animations.map((animation, index) => (
-          <LottieContainer key={index} />
+          <LottieContainer key={index} {...animation} />
         ))}
-      </ScrollView>
+      </Animated.ScrollView>
     </SafeAreaView>
   );
 }
@@ -24,13 +27,14 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 20,
-    padding: 20
+    padding: 20,
+    backgroundColor: 'white'
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center'
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 40
   }
 });
