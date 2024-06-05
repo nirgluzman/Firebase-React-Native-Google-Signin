2b150f0469e45c5b8a4550e15e7de566f06fcdd4
nirgluzman <nir.gluzman@gmail.com>
2024-06-05 21:29:06 +0200

    define an array to hold references to all animation files

---
+++ constants/lottieAnimations.js
@@ -0,0 +1,24 @@
+// reference to animation files.
+
+export const animations = [
+  {
+    name: 'Bubbles',
+    path: '../assets/bubbles.json'
+  },
+  {
+    name: 'Loading Spinner',
+    path: '../assets/loading.json'
+  },
+  {
+    name: 'Confetti',
+    path: '../assets/confetti.json'
+  },
+  {
+    name: 'Processing',
+    path: '../assets/processing.json'
+  },
+  {
+    name: 'Cheering Animation',
+    path: '../assets/cheeringAnimation.json'
+  }
+];
