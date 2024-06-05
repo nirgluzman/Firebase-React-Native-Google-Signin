3cf6bec0ab68100b55242807383fc63925ef8332
nirgluzman <nir.gluzman@gmail.com>
2024-06-05 21:33:07 +0200

    add basic rendering with map

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,36 @@
-import { Text, View } from 'react-native';
+import {
+  Text,
+  SafeAreaView, // render content within the safe area boundaries of a device (currently applicable only to iOS devices).
+  ScrollView,
+  StyleSheet
+} from 'react-native';
+
+import LottieContainer from '../components/LottieContainer';
+import { animations } from '../constants/lottieAnimations';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello, App is Ready !!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>A Glimpse to Lottie Animations</Text>
+        {animations.map((animation, index) => (
+          <LottieContainer key={index} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 20,
+    padding: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+});
